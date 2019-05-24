6bb875da814693ab4868944e034ae37a8f4c35f7
UsuallyChris <schoonc@gmail.com>
2019-05-24 14:28:52 -0700

    Added fonts and general global font styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,15 @@ import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// WEB FONT SETTINGS
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Rubik:400,700', 'Karla:400']
+  }
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
