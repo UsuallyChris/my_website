12d317117f47680a216da9c010d84d5781ec8aef
UsuallyChris <schoonc@gmail.com>
2019-05-24 13:56:38 -0700

    Changed out resume for socials. Resume not really needed.

---
+++ frontend/src/components/left/Contact.jsx
@@ -0,0 +1,8 @@
+/**
+ * Display component for the ContactCards
+ * 
+ * Connects to project state
+ *      Grabs all social models from api
+ *      passes down to each contact card
+ *      loops through state
+ */
---
+++ frontend/src/components/left/ContactCard.jsx
@@ -0,0 +1,8 @@
+/**
+ * Component for creating social cards
+ * 
+ * Takes in props for:
+ *      Color -> Just the hex code. Left side border. Should be pretty easy
+ *      Logo -> Use font awesome? Look up how to
+ *      Address -> My URL for the type of account 
+ */
