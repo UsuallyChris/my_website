5ce364672f9efdd86059912a72025759318f7098
UsuallyChris <schoonc@gmail.com>
2019-08-06 16:44:42 -0700

    Right side components removed and nav component file created.

--- src/components/right/RightContainer.jsx
+++ src/components/right/RightContainer.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
 // component imports
-import AboutMe from './AboutMe';
-import Projects from './Projects';
-import Skills from './Skills';
+
 
 class RightContainer extends Component {
   render() {
     return (
       <div className="right-container">
-        <AboutMe />
-        <Projects />
-        <Skills />
+        
       </div>
     )
   }
