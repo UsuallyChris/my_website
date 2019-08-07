5389f56d57ba080818476b22ce147eb9c634c4a6
UsuallyChris <schoonc@gmail.com>
2019-08-06 21:24:24 -0700

    Left side roughed in.

--- src/components/left/LeftContainer.jsx
+++ src/components/left/LeftContainer.jsx
@@ -7,8 +7,19 @@ class LeftContainer extends Component {
   render() {
     return (
       <div className="left-container">
-        <h1>Left Side</h1>
-        <Nav />
+        <div className="tag">
+          <h1>Hello, I'm Chris Schoonover.</h1>
+          <h1>I'm a developer based in San Diego.</h1>
+        </div>
+        <div className="nav-container">
+          <Nav />
+          <h2>Resume</h2>
+        </div>
+        <div className="socials-wrapper">
+          <p>GitHub</p>
+          <p>Twitter</p>
+          <p>Email</p>
+        </div>
       </div>
     )
   }
