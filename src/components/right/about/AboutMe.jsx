fbcf5e97e004aae146540e08b6c0e96c8f20c8c2
UsuallyChris <schoonc@gmail.com>
2019-08-09 16:53:01 -0700

    Have profile picture added and styled.

--- src/components/right/about/AboutMe.jsx
+++ src/components/right/about/AboutMe.jsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 // Component Imports
 import Page from '../common/Page';
 
+// Image Import
+import profile from '../../../resources/profile.jpg';
+
 class AboutMe extends Component {
   render() {
     return (
@@ -10,7 +13,12 @@ class AboutMe extends Component {
         <div>
           <h2>Who, me?</h2>
           <hr/>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?</p>
+          <div className="about-wrapper">
+            <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?</p>
+            <div className="profile-container">
+              <img className="profile" src={profile} alt=""/>
+            </div>  
+          </div>
         </div>
         <div>
           <h2>Technologies I've worked with lately:</h2>
