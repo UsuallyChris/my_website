5f9ddf81720a0173721366edb55f3faeb5e0f8dd
UsuallyChris <schoonc@gmail.com>
2019-08-08 22:17:59 -0700

    Projects rouged in and tag line updated.

--- src/components/left/LeftContainer.jsx
+++ src/components/left/LeftContainer.jsx
@@ -14,7 +14,7 @@ class LeftContainer extends Component {
     return (
       <div className="left-container">
         <div className="tag">
-          <h1>Hi! My name is <span className="tag-highlight">Chris Schoonover</span>.</h1>
+          <h1>Hi there! My name is <span className="tag-highlight">Chris Schoonover</span>.</h1>
           <h1>I build stuff for the web.</h1>
         </div>
         <div className="nav-wrapper">
--- src/components/right/projects/Projects.jsx
+++ src/components/right/projects/Projects.jsx
@@ -6,7 +6,16 @@ import Page from '../common/Page';
 function Projects() {
   return (
     <Page title="PROJECTS">
-
+      <div>
+        <h2>Personal Finance App</h2>
+        <hr/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?</p>
+      </div>
+      <div>
+        <h2>Job Application Tracker</h2>
+        <hr/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?</p>
+      </div>
     </Page>
   )
 }
