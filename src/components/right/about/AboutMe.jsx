c296f08075892f2ea744e982974e81a2eb9cfce0
UsuallyChris <schoonc@gmail.com>
2019-08-09 17:52:17 -0700

    Styling for list done.

--- src/components/right/about/AboutMe.jsx
+++ src/components/right/about/AboutMe.jsx
@@ -14,7 +14,11 @@ class AboutMe extends Component {
           <h2>Who, me?</h2>
           <hr/>
           <div className="about-wrapper">
-            <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?</p>
+            <div className="about-text">
+              <p>Hello! I'm Chris, a software engineer based in San Diego, CA who enjoys building things that find their home on the internet.</p>
+              <p>I recently graduated from San Diego State University and am currently looking for work in the San Diego area. I am open to remote positions.</p>
+              <p>Shoot me an email, message me on Twitter, or send me a smoke signal. Let's connect.</p>
+            </div>
             <div className="profile-container">
               <img className="profile" src={profile} alt=""/>
             </div>  
@@ -23,7 +27,18 @@ class AboutMe extends Component {
         <div>
           <h2>Technologies I've worked with lately:</h2>
           <hr/>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?</p>
+          <div className="tech-list">
+            <ul>
+              <li>Test 1</li>
+              <li>Test 2</li>
+              <li>Test 3</li>
+            </ul>
+            <ul>
+              <li>Test 1</li>
+              <li>Test 2</li>
+              <li>Test 3</li>
+            </ul>
+          </div>
         </div>
       </Page>
     )
