a8e0f9c69435bd52e93987cc43d87a1a4efd2d0f
UsuallyChris <schoonc@gmail.com>
2019-08-11 14:18:52 -0700

    Images loading on Projects page.

--- src/components/right/projects/ProjectCard.jsx
+++ src/components/right/projects/ProjectCard.jsx
@@ -6,6 +6,7 @@ function ProjectCard(props) {
       <h2>{props.title}</h2>
       <hr/>
       <div>
+        <img className="project-img" src={props.img} alt={props.alt}/>
         <p>{props.description}</p>
       </div>
     </div>
--- src/components/right/projects/Projects.jsx
+++ src/components/right/projects/Projects.jsx
@@ -4,15 +4,23 @@ import React from 'react';
 import Page from '../common/Page';
 import ProjectCard from './ProjectCard';
 
+// Image Imports
+import Bills from '../../../resources/bills.png';
+import Jobs from '../../../resources/jobs.png'
+
 function Projects() {
   return (
     <Page title="PROJECTS">
       <ProjectCard 
         title="Personal Finance App"
+        img={Bills}
+        alt="Personal Finance App Screenshot"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?"
       />
       <ProjectCard 
         title="Job Application Tracker"
+        img={Jobs}
+        alt="Job Application Tracker Screenshot"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium doloremque recusandae dignissimos expedita exercitationem quo, eaque sequi, libero porro sunt sit. Deleniti a in perspiciatis tempore amet adipisci laudantium?"
       />
     </Page>
