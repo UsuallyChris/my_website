3afb9bfddac89fcc07d8e469ce661130896e96d1
UsuallyChris <schoonc@gmail.com>
2019-08-12 14:34:51 -0700

    Initial mobile styling working.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/normalize.css';
 import './css/App.css';
 
@@ -10,18 +10,48 @@ import Projects from './components/right/projects/Projects';
 // React Router Imports
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <MainContainer>
-        <Switch>
-          <Route exact path='/' component={AboutMe} />
-          <Route path='/about' component={AboutMe} />
-          <Route path='/projects' component={Projects} />
-        </Switch>
-      </MainContainer>
-    </BrowserRouter>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth
+    }
+
+    this.handleWidnowSizeChange = this.handleWidnowSizeChange.bind(this);
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWidnowSizeChange);
+  }
+
+  handleWidnowSizeChange() {
+    this.setState({
+      width: window.innerWidth
+    })
+  }
+
+  render() {
+
+    const { width } = this.state;
+
+    if(width < 1300) {
+      return(
+        <h1>TEST</h1>
+      )
+    } else {
+      return (
+        <BrowserRouter>
+          <MainContainer>
+            <Switch>
+              <Route exact path='/' component={AboutMe} />
+              <Route path='/about' component={AboutMe} />
+              <Route path='/projects' component={Projects} />
+            </Switch>
+          </MainContainer>
+        </BrowserRouter>
+      );
+    }
+  }
 }
 
 export default App;
